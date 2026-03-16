2778377d84103b1884a136e54ded1cb998bb50ab
UltralyticsAssistant <web@ultralytics.com>
2026-03-16 04:18:42 +0000

    Auto-format by https://ultralytics.com/actions

--- demo_predict.py
+++ demo_predict.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
-yolo = YOLO("runs/detect/train/weights/best.pt", task = "detect")
-result = yolo(source=0,show = True)
+
+yolo = YOLO("runs/detect/train/weights/best.pt", task="detect")
+result = yolo(source=0, show=True)
